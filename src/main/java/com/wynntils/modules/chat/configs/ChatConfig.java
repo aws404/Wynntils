ab3d3d61dfd61889acdcab0e2ca6522cfb277e4c
aws404 <aws40404@gmail.com>
2020-06-18 23:53:08 +1000

    Chat mention nicknames

--- src/main/java/com/wynntils/modules/chat/configs/ChatConfig.java
+++ src/main/java/com/wynntils/modules/chat/configs/ChatConfig.java
@@ -30,6 +30,9 @@ public class ChatConfig extends SettingsClass {
     @Setting(displayName = "Chat Mentions", description = "Should a sound play when your username appears in chat?")
     public boolean allowChatMentions = true;
 
+    @Setting(displayName = "Chat Mention Nicknames", description = "List of additional names that trigger a notification. Seperated by commas")
+    public String mentionNames = "";
+
     @Setting(displayName = "Chat Spam Filter", description = "Should repeating messages stack rather than flood the chat?")
     public boolean blockChatSpamFilter = true;
 
